3a6acfb0086adc8ccaf20dac3230408587e83900
suzuki <ruki98733789@gmail.com>
2025-06-07 07:29:18 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  get "pages/about"
+  root "pages#homepage"
+  resources :ideas
+  
+  # get "/ideas" => "rails/ideas#index"
+  # get "/idea/new" => "rails/ideas#new"
+  # get "/idea/:idea_id/" => "rails/ideas#show"
+  # get "/idea/:idea_id/edit" => "rails/ideas#edit"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
